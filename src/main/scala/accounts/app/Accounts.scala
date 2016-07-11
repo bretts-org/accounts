54d15b138b64c32cb963ef3399fa05f3bb565f99
Andrew Brett <git@bretts.org>
2016-07-11 10:04:02 +0100

    Add logging config, and log on app startup

--- src/main/scala/accounts/app/Accounts.scala
+++ src/main/scala/accounts/app/Accounts.scala
@@ -6,10 +6,11 @@ import accounts.model.ShellModel
 import accounts.record.repository.file.FileRecordRepository
 import accounts.view.ShellView
 import accounts.viewmodel.ShellViewModel
+import com.typesafe.scalalogging.StrictLogging
 
 import scalafx.application.JFXApp
 
-object Accounts extends JFXApp {
+object Accounts extends JFXApp with StrictLogging {
   val transFileName = parameters.named.getOrElse("transfile",
     throw new IllegalArgumentException("Missing argument: --transfile"))
   val transFile = new File(transFileName)
@@ -18,6 +19,8 @@ object Accounts extends JFXApp {
   val model = new ShellModel(recordRepo)
   val viewModel = new ShellViewModel(model)
   val view = new ShellView(viewModel)
+
+  logger.info("Starting application")
 
   stage = view.stage
 }
