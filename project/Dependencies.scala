60adc21bbd5b64f1fbd3a6f44707d8de7aaa575d
Andrew Brett <git@bretts.org>
2016-08-16 09:20:21 +0100

    Upgrade to latest dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,13 +12,13 @@ object Dependencies {
     "com.typesafe.scala-logging" %% "scala-logging" % "3.4.0",
     "ch.qos.logback" % "logback-classic" % "1.1.7",
 
-    "com.beachape" %% "enumeratum" % "1.4.8",
+    "com.beachape" %% "enumeratum" % "1.4.9",
     "com.github.tototoshi" %% "scala-csv" % "1.3.3",
     "org.scalafx" %% "scalafx" % "8.0.92-R10",
-    "org.scalactic" %% "scalactic" % "3.0.0-RC4",
+    "org.scalactic" %% "scalactic" % "3.0.0",
     "org.typelevel" %% "cats" % "0.6.1",
 
-    "org.scalatest" %% "scalatest" % "3.0.0-RC4" % Test,
+    "org.scalatest" %% "scalatest" % "3.0.0" % Test,
     "org.mockito" % "mockito-core" % "2.0.79-beta"% Test,
 
     "io.scalatestfx" %% "scalatestfx" % "0.0.2-alpha" % UiTest.UiTest,
